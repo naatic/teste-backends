5f29cad49989ba3a426d95813bcd38fc358e424a
Natália Costa <natalia.costa@bcredi.com.br>
2020-01-16 09:50:05 -0300

    elixir implementation

--- elixir/lib/application/service/proponent_service.ex
+++ elixir/lib/application/service/proponent_service.ex
@@ -3,9 +3,9 @@ defmodule LoanHandler.Application.Service.ProponentService do
     def is_proponents_valid?(proponents_list, proposal) do
 
         is_only_one_main_proponent?(proponents_list) and
-        is_proposal_proponents_valid?(proponents_list, proposal) and
         is_number_of_proponents_enough?(proponents_list) and
-        is_proponent_age_enough?(proponents_list)
+        is_proponent_age_enough?(proponents_list) and
+        is_proposal_proponents_valid?(proponents_list, proposal)
 
     end
 
@@ -23,12 +23,7 @@ defmodule LoanHandler.Application.Service.ProponentService do
             end
         end
 
-        unless length(main_prop) > 1 do
-            true
-        else
-            false
-        end
-
+        length(main_prop) > 1
     end
 
     defp is_proposal_proponents_valid?(proponents_list, proposal) do
@@ -37,13 +32,13 @@ defmodule LoanHandler.Application.Service.ProponentService do
 
         case main_proponent do
             proponent_age when proponent_age >= 18 and proponent_age <= 24 ->
-                (proposal.proposal_monthly_payment * 4) <= main_proponent.proponent_monthly_income
+                (proposal_monthly_payment * 4) <= main_proponent.proponent_monthly_income
 
             proponent_age when proponent_age > 24 and proponent_age <= 50 ->
-                (proposal.proposal_monthly_payment * 3) <= main_proponent.proponent_monthly_income
+                (proposal_monthly_payment * 3) <= main_proponent.proponent_monthly_income
 
             proponent_age when proponent_age > 50 ->
-                (proposal.proposal_monthly_payment * 2) <= main_proponent.proponent_monthly_income
+                (proposal_monthly_payment * 2) <= main_proponent.proponent_monthly_income
 
         end
     end
--- elixir/lib/application/service/warranty_service.ex
+++ elixir/lib/application/service/warranty_service.ex
@@ -2,21 +2,33 @@ defmodule LoanHandler.Application.Service.WarrantyService do
 
     def is_warranties_valid?(warranties_list, proposal) do
 
-        is_all_warranties_valid?(warranties_list) and
+        is_warranties_provinces_valid?(warranties_list) and
         is_warranty_quantity_sufficient?(warranties_list) and
-        is_warranty_values_sum_sufficient?(warranties_list, proposal)
-    
+        is_warranty_price_sum_sufficient?(warranties_list, proposal)
+
     end
 
-    defp is_all_warranties_valid?(warranties_list) do
-        warranties_list
+    defp is_warranties_provinces_valid?(warranties_list) do
+       Enum.any?(warranties_list,
+            fn w ->
+                w.warranty_province != "PR" and
+                w.warranty_province != "SC" and
+                w.warranty_province != "RS"
+            end
+        )
     end
 
     defp is_warranty_quantity_sufficient?(warranties_list) do
-        warranties_list
+        length(warranties_list) >= 1
     end
 
-    defp is_warranty_values_sum_sufficient?(warranties_list, proposal) do
-        warranties_list
+    defp is_warranty_price_sum_sufficient?(warranties_list, proposal) do
+        warranty_price_amount = 0
+
+        for warranty <- warranties_list do
+            warranty_price_amount ++ warranty.warranty_value
+        end
+
+        warranty_price_amount >= proposal.proposal_loan_value * 2
     end
 end
